dfeb15a85f9e9f6a8c0bb23ff823d4edebafcdf2
Jānis Basovs <janis_basis@hotmail.com>
2023-06-04 01:49:34 +0300

    Add - Can now fetch file_collections from our API

--- app/api/documents/route.ts
+++ app/api/documents/route.ts
@@ -1,8 +1,26 @@
+import { CosmosClient } from "@azure/cosmos"
 import { NextResponse } from "next/server"
 
 export async function GET(_request: Request) {
+  const client = new CosmosClient({
+    endpoint: process.env.COSMOS_URL!,
+    key: process.env.COSMOS_KEY!,
+  })
+
+  const database = client.database(process.env.DATABASE_ID!)
+  const container = database.container(process.env.CONTAINER_ID!)
+
+  // Query data
+  const querySpec = {
+    query: "SELECT * from database_id",
+  }
+
+  const { resources: file_collections } = await container.items
+    .query(querySpec)
+    .fetchAll()
+
   return NextResponse.json(
-    { team: "Hoot & Hawk - All Documents", documents: [] },
+    { team: "Hoot & Hawk - All Documents", file_collections },
     { status: 200 }
   )
 }
