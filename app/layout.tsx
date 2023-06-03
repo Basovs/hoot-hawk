7d977707900f2731cf1138cf52d6e4ab1ae20931
Jānis Basovs <janis_basis@hotmail.com>
2023-06-04 02:17:43 +0300

    Add logo, Add tooltips for files

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import { BirdIcon } from "lucide-react"
 import "./globals.css"
 import { Inter } from "next/font/google"
 
@@ -17,6 +18,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <main className="flex flex-col items-center min-h-screen p-4">
+          <BirdIcon size={100} />
           <h1 className="text-6xl font-black mb-10">Hook & Hawk</h1>
 
           {children}
--- components/file-collection-list.tsx
+++ components/file-collection-list.tsx
@@ -1,6 +1,21 @@
+import { PaperclipIcon } from "lucide-react"
+import { Button } from "./ui/button"
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "./ui/tooltip"
+
+interface File {
+  id: string
+  file_name: string
+}
+
 interface FileCollection {
   id: string
   collection_name: string
+  files: File[]
 }
 
 interface FileColelctionListProps {
@@ -27,11 +42,30 @@ interface FileCollectionProps {
 }
 
 const FileCollection = ({ fileCollection }: FileCollectionProps) => {
-  const { collection_name } = fileCollection
+  const { collection_name, files } = fileCollection
 
   return (
-    <div className="p-4 rounded border">
+    <div className="px-4 py-2 rounded border flex justify-between items-center">
       <p>{collection_name}</p>
+
+      <div className="flex items-center">
+        {files.map((file) => {
+          return (
+            <TooltipProvider>
+              <Tooltip>
+                <TooltipTrigger asChild>
+                  <Button size="sm" variant="ghost">
+                    <PaperclipIcon key={file.id} size={14} />
+                  </Button>
+                </TooltipTrigger>
+                <TooltipContent>
+                  <p>{file.file_name}</p>
+                </TooltipContent>
+              </Tooltip>
+            </TooltipProvider>
+          )
+        })}
+      </div>
     </div>
   )
 }
