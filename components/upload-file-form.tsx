9edadd3cdbd5106316d4db78c05e2903f7820deb
Jānis Basovs <janis_basis@hotmail.com>
2023-06-04 23:28:14 +0300

    Update headers

--- components/upload-file-form.tsx
+++ components/upload-file-form.tsx
@@ -90,6 +90,9 @@ export function UploadFileForm() {
         {
           headers: {
             "Content-Type": "application/json",
+            Origin: "https://hoot-hawk.vercel.app/", // Replace with the actual origin/domain
+            "Access-Control-Request-Method": "POST",
+            "Access-Control-Request-Headers": "Content-Type",
           },
         }
       )
